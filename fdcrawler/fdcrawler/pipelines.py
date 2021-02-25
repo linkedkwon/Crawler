652ab8843cff8ddbb6502b7aaee546068c9b3502
linkedkwon <linkedkwon@gmail.com>
2021-02-25 23:28:40 +0900

    Add pipeline to process data scraped into csv

--- fdcrawler/fdcrawler/pipelines.py
+++ fdcrawler/fdcrawler/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exporters import CsvItemExporter, XmlItemExporter, JsonItemExporter
 
 
-class FdcrawlerPipeline:
+class CsvPipeline(object):
+    def __init__(self):
+        self.file = open("assets/detail.csv", 'wb')
+        self.exporter = CsvItemExporter(self.file, encoding='utf-8-sig')
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
